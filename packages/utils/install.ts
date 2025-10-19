539737756bb1bbaf300ce5a271f8a57fd926ab45
杨文龙 <3111018605@qq.com>
2025-10-19 11:49:41 +0800

    feat(workspace): 初始化项目结构和基础配置

---
+++ packages/utils/install.ts
@@ -0,0 +1,18 @@
+import type { App, Plugin } from "vue";
+import { each } from "lodash-es";
+
+type SFCWithInstall<T> = T & Plugin;
+
+export function makeInstaller(components: Plugin[]) {
+  const installer = (app: App) => each(components, (c) => app.use(c));
+
+  return installer as Plugin;
+}
+
+export const withInstall = <T>(component: SFCWithInstall<T>) => {
+  component.install = (app: App) => {
+    app.component((component as any).name, component as Plugin);
+  };
+
+  return component;
+};
